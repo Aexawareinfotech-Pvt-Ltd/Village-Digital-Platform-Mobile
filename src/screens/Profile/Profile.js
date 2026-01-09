2226f7cdc25133522b4b6b7be674afc2825a19d1
Naitik <naitikpatel1205@gmail.com>
2026-01-09 12:11:20 +0530

    Resolve Confilt

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -82,7 +82,6 @@ export default function Profile() {
   // Navigation Handler
   const handleMenuPress = (item) => {
     if (item.isLogout) {
-<<<<<<< Updated upstream
       if (Platform.OS === "web") {
       navigation.navigate("Login");
       console.log("User logged out (WEB)");
@@ -93,19 +92,6 @@ export default function Profile() {
         { text: 'Cancel', style: 'cancel' },
         { text: 'Logout', style: 'destructive', onPress: () => {
           navigation.navigate('Login');
-=======
-      if (Platform.OS === 'web') {
-        navigation.navigate('Login');
-        console.log('Navigate to Login (Web)');
-        return;
-      }
-      
-      Alert.alert('Logout', 'Are you sure you want to logout?', [
-        { text: 'Cancel', style: 'cancel' },
-        { text: 'Logout', style: 'destructive', onPress: () => {
-          
-          navigation.replace('Login') 
->>>>>>> Stashed changes
         }}
       ]);
       return;
